fee518e99452e1cb6b936d75d7ebc5ebc174f570
Patryk Nowak <papknight@gmail.com>
2020-11-27 20:34:07 +0100

    change concept

--- LOTTO_Simulator.py
+++ LOTTO_Simulator.py
@@ -1,11 +1,28 @@
-def user():
+# def user():
+#     numbers = 0
+#     my_list = []
+#     while numbers < 6:
+#         try:
+#             my_list.append(int(input(f"Podaj {numbers+1} liczbę: ")))
+#             numbers += 1
+#         except ValueError:
+#             print("To nie jest liczba")
+#     return my_list
+
+def compare():
     numbers = 0
     my_list = []
     while numbers < 6:
         try:
-            my_list.append(int(input(f"Podaj {numbers+1} liczbę: ")))
-            numbers += 1
+            a = int(input(f"Podaj {numbers+1} liczbę: "))
+            if a in range(1, 50) and a not in my_list:
+                my_list.append(a)
+                numbers += 1
+            else:
+                print("Podaj liczbę z przedziału 1 - 49")
         except ValueError:
-            print("To nie jest liczba")
+            print("Proszę podać liczbę: ")
     print(my_list)
-user()
+
+
+compare()
